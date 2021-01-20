80f5b63455af709f96dab6382eb94a2f122788d6
tpcred <tpcred1@gmail.com>
2021-01-20 13:09:18 +0000

    add store side auction

--- simulator/store.py
+++ simulator/store.py
@@ -5,6 +5,7 @@ from pade.acl.aid import AID
 from pade.behaviours.protocols import FipaSubscribeProtocol
 from pade.behaviours.protocols import TimedBehaviour
 from pade.behaviours.protocols import FipaRequestProtocol
+from pade.behaviours.protocols import FipaContractNetProtocol
 
 import datetime
 import random
@@ -12,6 +13,83 @@ import random
 import utils
 import macros
 import epgp_table
+
+class RewardAuction(FipaContractNetProtocol):
+    # start auction for rewards
+    def __init__(self, agent):
+        super(RewardAuction, self).__init__(agent=agent, message=None, is_initiator=True)
+        self.number_of_total_rewards = len(self.agent.rewards_list)
+        self.number_of_rewars_handled = 0
+
+
+    def handle_all_proposes(self, proposes):
+        super(RewardAuction, self).handle_all_proposes(proposes)
+
+        best_proposer = None
+        higher_epgp = 0.0
+        other_proposers = list()
+        display_message(self.agent.aid.name, 'Analyzing proposals...')
+
+        for message in proposes:
+            content = message.content
+            epgp = float(content)
+            display_message(self.agent.aid.name,
+                            'Analyzing proposal {i}'.format(i=i))
+            display_message(self.agent.aid.name,
+                            'Ep/gp ratio: {pot}'.format(pot=epgp))
+            i += 1
+            if epgp > higher_epgp:
+                if best_proposer is not None:
+                    other_proposers.append(best_proposer)
+
+                higher_epgp = epgp
+                best_proposer = message.sender
+            else:
+                other_proposers.append(message.sender)
+
+        display_message(self.agent.aid.name,
+                        'The best proposal was: {pot} ep/gp ratio'.format(
+                            pot=higher_epgp))
+
+        if other_proposers != []:
+            display_message(self.agent.aid.name,
+                            'Sending REJECT_PROPOSAL answers...')
+            answer = ACLMessage(ACLMessage.REJECT_PROPOSAL)
+            answer.set_protocol(ACLMessage.FIPA_CONTRACT_NET_PROTOCOL)
+            answer.set_content('')
+            for agent in other_proposers:
+                answer.add_receiver(agent)
+
+            self.agent.send(answer)
+
+        if best_proposer is not None:
+            display_message(self.agent.aid.name,
+                            'Sending ACCEPT_PROPOSAL answer...')
+
+            answer = ACLMessage(ACLMessage.ACCEPT_PROPOSAL)
+            answer.set_protocol(ACLMessage.FIPA_CONTRACT_NET_PROTOCOL)
+            answer.set_content('OK')
+            answer.add_receiver(best_proposer)
+            self.agent.send(answer)
+
+
+    def handle_inform(self, message):
+        if message.ontology in self.agent.rewards_list_name:
+            super(RewardAuction, self).handle_inform(message)
+
+            display_message(self.agent.aid.name, 'INFORM message received')
+
+    def handle_refuse(self, message):
+        if message.ontology in self.agent.rewards_list_name:
+            super(RewardAuction, self).handle_refuse(message)
+
+            display_message(self.agent.aid.name, 'REFUSE message received')
+
+    def handle_propose(self, message):
+        if message.ontology in self.agent.rewards_list_name:
+            super(RewardAuction, self).handle_propose(message)
+
+            display_message(self.agent.aid.name, 'PROPOSE message received')
 
 class RegisterWorkers(FipaSubscribeProtocol):
 
@@ -50,42 +128,49 @@ class PassTime(TimedBehaviour):
         self.create_shifts_hour = 5
         self.create_shifts_day = 1
 
+
     def on_time(self):
         super(PassTime, self).on_time()
-        self.agent.date = self.agent.date + datetime.timedelta(hours=1)
+        if self.agent.stop_time == 0:
+            self.agent.date = self.agent.date + datetime.timedelta(hours=1)
 
-        if self.agent.date.hour == self.create_shifts_hour and self.agent.date.day == self.create_shifts_day:
-            self.createShifts()
+            if self.agent.date.hour == self.create_shifts_hour and self.agent.date.day == self.create_shifts_day:
+                self.createShifts()
 
-        if self.agent.date.hour == 6:
-            self.setShifts(self.agent.date.day)
-        elif self.agent.date.hour == 8:
-            self.startEndShift(self.agent.morning_shift)
-        elif self.agent.date.hour == 16:
-            self.startEndShift(self.agent.morning_shift)
-            self.startEndShift(self.agent.afternoon_shift)
-        elif self.agent.date.hour == 0:
-            self.startEndShift(self.agent.afternoon_shift)
-            self.closeStore()
-        message = ACLMessage(ACLMessage.INFORM)
-        message.set_protocol(ACLMessage.FIPA_SUBSCRIBE_PROTOCOL)
-        message.set_content(self.agent.date)
-        self.notify(message)
+            if self.agent.date.hour == 6:
+                self.setShifts(self.agent.date.day)
+            elif self.agent.date.hour == 8:
+                self.startEndShift(self.agent.morning_shift)
+            elif self.agent.date.hour == 16:
+                self.startEndShift(self.agent.morning_shift)
+                self.startEndShift(self.agent.afternoon_shift)
+            elif self.agent.date.hour == 0:
+                self.startEndShift(self.agent.afternoon_shift)
+                self.closeStore()
 
-        # Start distributing tasks
-        display_message(self.agent.aid.localname, "Starting distributing tasks")
-        current_shift = self.agent.get_current_shift()
-       
-        if current_shift != None:
-            display_message(self.agent.aid.localname, current_shift)
-        
-            for worker_name in current_shift:
-                message = ACLMessage(ACLMessage.REQUEST)
-                message.set_protocol(ACLMessage.FIPA_REQUEST_PROTOCOL)
-                message.set_ontology(macros.TASK_ONTOLOGY)
-                message.add_receiver(worker_name)
-                message.set_content(str(self.agent.date))
-                self.agent.send(message)
+            if self.agent.date.hour == 0 and self.agent.date.day == 1:
+                self.agent.stop_time = 1
+                self.startAuction(self.agent.rewards_list)
+
+            message = ACLMessage(ACLMessage.INFORM)
+            message.set_protocol(ACLMessage.FIPA_SUBSCRIBE_PROTOCOL)
+            message.set_content(self.agent.date)
+            self.notify(message)
+
+            # Start distributing tasks
+            display_message(self.agent.aid.localname, "Starting distributing tasks")
+            current_shift = self.agent.get_current_shift()
+
+            if current_shift != None:
+                display_message(self.agent.aid.localname, current_shift)
+
+                for worker_name in current_shift:
+                    message = ACLMessage(ACLMessage.REQUEST)
+                    message.set_protocol(ACLMessage.FIPA_REQUEST_PROTOCOL)
+                    message.set_ontology(macros.TASK_ONTOLOGY)
+                    message.add_receiver(worker_name)
+                    message.set_content(str(self.agent.date))
+                    self.agent.send(message)
 
 
     def createShifts(self):
@@ -144,6 +229,16 @@ class PassTime(TimedBehaviour):
             message.set_content('Change work')
             self.agent.send(message)
 
+    def startAuction(self, items):
+
+        for item in items:
+            message = ACLMessage(ACLMessage.CFP)
+            message.set_protocol(ACLMessage.FIPA_CONTRACT_NET_PROTOCOL)
+            message.set_content(item)
+
+            for worker in self.agent.workers_aid:
+                message.add_receiver(worker)
+
 
 
 class DisplayStoreInfo(TimedBehaviour):
@@ -189,7 +284,12 @@ class Store(Agent):
         self.schedule =  list()
         self.epgp = epgp_table.Epgp_table()
 
-        self.date = datetime.datetime(2020, 1, 1)
+        self.date = datetime.datetime(2020, 1, 1) + datetime.timedelta(hours=1)
+
+        self.rewards_list = list()
+        self.rewards_list_name: = list()
+
+        self.stop_time = 0
 
         self.register = RegisterWorkers(self)
         self.behaviours.append(self.register)
@@ -201,7 +301,10 @@ class Store(Agent):
 
         self.distribute_tasks = DistributeTasks(self)
         self.behaviours.append(self.distribute_tasks)
-        
+
+        self.reward_auction = RewardAuction(self)
+        self.behaviours.append(self.reward_auction)
+
 
     def launch_pass_time(self):
         self.pass_time = PassTime(self, 1.0, self.register.notify)
@@ -211,17 +314,14 @@ class Store(Agent):
     def on_start(self):
         super(Store, self).on_start()
         display_message(self.aid.localname, 'store initalized')
-    
+
 
     def get_current_shift(self):
-        
+
         if self.date.hour >= 8 and self.date.hour < 16:
             return self.morning_shift
-    
         elif self.date.hour >= 16:
             return self.afternoon_shift
-
         else:
             return []
 
-    
